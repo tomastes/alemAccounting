9a1a7ff4fa809ea00b289e8bd7e1948d11aa21cb
tomas tesfamariam <tomas@pharox.io>
2023-02-13 17:12:40 +0100

    bug fixes

---
+++ src/fbConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAUwTPuvQqbpMEZstk3eeHP5LF4CUd0sN0",
+  authDomain: "alemaccounting.firebaseapp.com",
+  projectId: "alemaccounting",
+  storageBucket: "alemaccounting.appspot.com",
+  messagingSenderId: "230741309052",
+  appId: "1:230741309052:web:d508854e7b967916d83bf9",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
