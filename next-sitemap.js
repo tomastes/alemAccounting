b3d7b11fd687dd62ff337440f03532acc91ab2d8
tomas tesfamariam <tomas@pharox.io>
2023-02-28 15:16:18 +0100

    site map fix

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: 'https://startup-app-modern.vercel.app',
+  siteUrl: 'https://www.alemaccounting.com/',
   generateRobotsTxt: true,
   // optional
   // robotsTxtOptions: {
