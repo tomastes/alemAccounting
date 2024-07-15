b0ccc62f6ad936884b29afdce60a4a5229bebb4c
tomas tesfamariam <tomasberhe1999@gmail.com>
2024-07-15 21:50:45 +0200

    seo added

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,41 +1,50 @@
 import React from "react";
 import Head from "next/head";
 
-export default function SEO({ description = "ALEM ACCOUNTING", author = "TOMASWEBSOLUTIONS", meta, title = "ACCOUNTING" }) {
+export default function SEO({ description = "Admas RijSchool in Leeuwarden biedt eersteklas rijlessen aan.", author = "Admas RijSchool", meta = [], title = "ADMAS RIIJSCHOOL - Leeuwarden" }) {
   const metaData = [
     {
-      name: `description`,
+      name: "description",
       content: description,
     },
     {
-      property: `og:title`,
+      property: "og:title",
       content: title,
     },
     {
-      property: `og:description`,
+      property: "og:description",
       content: description,
     },
     {
-      property: `og:type`,
-      content: `website`,
+      property: "og:type",
+      content: "website",
     },
     {
-      name: `twitter:card`,
-      content: `summary`,
+      property: "og:locale",
+      content: "nl_NL",
     },
     {
-      name: `twitter:creator`,
+      property: "og:site_name",
+      content: "Admas RijSchool",
+    },
+    {
+      name: "twitter:card",
+      content: "summary",
+    },
+    {
+      name: "twitter:creator",
       content: author,
     },
     {
-      name: `twitter:title`,
+      name: "twitter:title",
       content: title,
     },
     {
-      name: `twitter:description`,
+      name: "twitter:description",
       content: description,
     },
   ].concat(meta);
+
   return (
     <Head>
       <title>{title}</title>
@@ -47,6 +56,7 @@ export default function SEO({ description = "ALEM ACCOUNTING", author = "TOMASWE
 }
 
 SEO.defaultProps = {
-  lang: `en`,
+  lang: "en",
   meta: [],
+  description: "Admas RijSchool in Leeuwarden biedt eersteklas rijlessen aan.",
 };
